c9db74cc572db876d1579a9d5d0ef900892217a8
Astra-chan-desu <lyosha5438@gmail.com>
2025-05-29 18:14:28 +0300

    firstone

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,55 @@
 // Copyright 2025 UNN-CS
 #include <cstdint>
 #include "alg.h"
+#include <cmath>
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if(value <2){
+    return false;
+  }
+  if(value ==3||value==2){
+    return true;
+  }else  if(value%2 == 0) return false;
+  for (uint64_t i = 3; pow(i, 2)<value; i+=2){
+      if(value%i == 0) return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
-}
+    uint64_t counter = 1;
+    uint64_t number = 1;
+  if(n<2) return 0;
+  if(n==2) return 1;
+  while(counter < n){
+    number+=2;
+    if(checkPrime(number)) counter++;
+  }
 
+  return number;
+}
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  if(value<2) return 2;
+  if(value==2) return 3;
+  if(value%2 == 0) value--;
+  while(true){
+    value+=2;
+    if(checkPrime(value)) return value;
+  }
+
+  return -1;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+    uint64_t number = 3;
+    uint64_t sum = 2;
+  if(hbound<3) return 2;
+  if(hbound<5) return 5;
+  while(number < hbound){
+    if(checkPrime(number)) sum+=number;
+        number+=2;
+  }
+
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -5,6 +5,71 @@
 #include "alg.h"
 
 
+TEST(st1_strakhov, checkPrime1) {
+  bool res = checkPrime(1);
+  bool expected = false;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1_strakhov, checkPrime2) {
+  bool res = checkPrime(2);
+  bool expected = true;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1_strakhov, checkPrime3) {
+  bool res = checkPrime(3);
+  bool expected = true;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1_strakhov, checkPrime4) {
+  bool res = checkPrime(4);
+  bool expected = false;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1_strakhov, nPrime1) {
+  uint64_t res = nPrime(1);
+  uint64_t expected = 2;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1_strakhov, nPrime2) {
+  uint64_t res = nPrime(2);
+  uint64_t expected = 3;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1_strakhov, nPrime3) {
+  uint64_t res = nPrime(3);
+  uint64_t expected = 5;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1_strakhov, nPrime4) {
+  uint64_t res = nPrime(4);
+  uint64_t expected = 7;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1_strakhov, nextPrime0) {
+  uint64_t res = nextPrime(0);
+  uint64_t expected = 2;
+  EXPECT_EQ(expected, res);
+}
+
+TEST(st1_strakhov, nextPrime2) {
+  uint64_t res = nextPrime(2);
+  uint64_t expected = 3;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1_strakhov, nextPrime3) {
+  uint64_t res = nextPrime(3);
+  uint64_t expected = 5;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1_strakhov, nextPrime12) {
+  uint64_t res = nextPrime(12);
+  uint64_t expected = 13;
+  EXPECT_EQ(expected, res);
+}
+
 TEST(st1, sumPrime1) {
   uint64_t res = sumPrime(2000000);
   uint64_t expected = 142913828922;
